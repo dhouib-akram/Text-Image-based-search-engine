39ae337a3aad6d5e9f0258f737c7aa408ca08e6a
dhouib-akram <akram.dhouib@supcom.tn>
2023-10-12 17:00:04 +0100

    updated utils

---
+++ utils/config.py
@@ -0,0 +1,7 @@
+# File paths
+
+class_description_path='/content/drive/MyDrive/SearchEngineData/class-descriptions.csv'
+annotations_path='/content/drive/MyDrive/SearchEngineData/train-annotations-human-imagelabels.csv'
+images_path='/content/drive/MyDrive/SearchEngineData/train-images-with-labels-with-rotation.csv'
+merged_df_path='/content/drive/MyDrive/SearchEngineData/data_4M.csv'
+final_path="/content/drive/MyDrive/SearchEngineData/final.csv"
