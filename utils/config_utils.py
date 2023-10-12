8ca54e1a18746321f8f0e655fc3095a0368c97be
dhouib-akram <akram.dhouib@supcom.tn>
2023-10-12 22:28:21 +0100

    updated config

--- utils/config_utils.py
+++ utils/config_utils.py
@@ -5,3 +5,7 @@ annotations_path='/content/drive/MyDrive/SearchEngineData/train-annotations-huma
 images_path='/content/drive/MyDrive/SearchEngineData/train-images-with-labels-with-rotation.csv'
 merged_df_path='/content/drive/MyDrive/SearchEngineData/data_4M.csv'
 final_path="/content/drive/MyDrive/SearchEngineData/final.csv"
+url_filtered_path="/content/drive/MyDrive/SearchEngineData/perfect/perfect90k.csv"
+num_processes = 2
+start=0
+end=1000000
