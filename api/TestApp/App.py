1a26f5ec250b1a730fa86c8439e685232a387313
dhouib-akram <akram.dhouib@supcom.tn>
2023-10-15 20:42:54 +0100

    search_functions

--- api/TestApp/App.py
+++ api/TestApp/App.py
@@ -1,58 +1,16 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from utils.feature_extraction import FeatureExtractor
-from fastapi import Request , File, UploadFile
-import utils.config as config
-from PIL import Image
-import numpy as np
-from elasticsearch import Elasticsearch
+from fastapi import FastAPI
 
+from fastapi import Request 
+from elasticsearch import Elasticsearch
+import utils.search_functions as sf
 
 app = FastAPI()
-fe = FeatureExtractor(config.pca_path)
 client = Elasticsearch("https://c8ff-197-26-210-162.ngrok-free.app:443" )
 
 def check_server(client):
     if client.ping() == False:
         print("Elastic Search Server Is Not Running!")
 
-def get_results_search_by_text(content):
-        tags=content['tags']
-
-        search_body = {
-            "query": {
-                "bool": {
-                    "must": [
-                        {"match": {"Tags": tags}},
-                    ]
-                }
-            }
-        }
-        results=client.search(index=config.index_name, body=search_body, size=10)
-        return results
-
-def get_results_search_by_url(content):
-    url = content["url"]
-
-    feature = fe.get_from_link(url)
-    # Now you can use the 'url' variable to perform any desired actions
-    # Define your Elasticsearch query
-    query = {
-                    "knn": {
-                        "field": "FeatureVector",
-                        "query_vector": feature,
-                        "k": 10,
-                        "num_candidates": 100,
-                        "filter": {
-                            "multi_match": {
-                            "query": query,
-                            "fields":["Tags","Title"]
-                        },
-                }
-        }}
-    # Execute the Elasticsearch search
-    search = client.search(index=config.index_name, body={"query": query}, size=10)
-    return search
 @app.get('/')
 async def root():
     return {"Welcome to our Project"}
@@ -60,19 +18,26 @@ async def root():
 @app.post('/search_by_text')
 async def search_by_text(request:Request):
     content = await request.json()  
-    # Your Elasticsearch search query
-    results=get_results_search_by_text(content)
-    
-    return{"resulttype":results}
+    tags=content['tags']
 
-@app.post('/get_feature_from_url/')
+    return sf.get_results_search_by_text(tags)
 
-async def get_feature_from_url(request: Request):
+@app.post('/search_by_url/')
+
+async def search_by_url(request: Request):
     content = await request.json()
     
-    results=get_results_search_by_url(content)
+    url = content["url"]
 
-    return {"feature": results}
+    return sf.get_results_search_by_url(url)
+@app.post('/search_by_img/')
+
+async def search_by_img(request: Request):
+    content = await request.json()
+    
+    url = content["url"]
+
+    return sf.get_results_search_by_url(url)
 
 if __name__ == "__main__":
     import uvicorn
---
+++ api/TestApp/utils/search_functions.py
@@ -0,0 +1,30 @@
+from utils.feature_extraction import FeatureExtractor
+import utils.config as config
+from elasticsearch import Elasticsearch
+
+fe = FeatureExtractor(config.pca_path)
+client = Elasticsearch("https://c8ff-197-26-210-162.ngrok-free.app:443" )
+def get_results_search_by_text(tags):
+    search_body = {
+    "query": {
+            "bool": {
+                "must": [
+                    {"match": {"Tags": tags}},
+                        ]
+                    }
+                }
+            }
+    return client.search(index=config.index_name, body=search_body, size=10)
+def get_results_search_by_url(tags):
+
+    feature = fe.get_from_link(url)
+
+    query = {
+        "knn": {
+        "field": "FeatureVector",
+        "query_vector": feature,
+        "k": 10,
+        "num_candidates": 100
+     }
+    }
+    return client.search(index=config.index_name, body={"query": query}, size=10)
